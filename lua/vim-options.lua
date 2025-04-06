e84dc1eb6237958bb0a9da112b9c29a4482db4ee
Stefan Boudri <stefan.boudri@hva.nl>
2025-04-06 22:08:35 +0200

    initial commit

---
+++ lua/vim-options.lua
@@ -0,0 +1,7 @@
+vim.opt.number = true                -- Show line numbers
+vim.opt.relativenumber = true        -- Show relative line numbers
+vim.opt.tabstop = 4                  -- Set tab width
+vim.opt.shiftwidth = 4               -- Set indentation width
+vim.opt.expandtab = true             -- Use spaces instead of tabs
+vim.opt.smartindent = true           -- Enable smart indentation
+vim.g.mapleader = " "

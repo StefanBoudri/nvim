6bc8fdce13f78b4e939362046bf86beb47be9f3b
Stefan Boudri <stefan.boudri@hva.nl>
2025-04-09 00:43:05 +0200

    omnisharp lsp fix

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,36 +1,54 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "omnisharp", "clangd", "eslint", "jdtls", "jsonls", "markdown_oxide" },
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		lazy = false,
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end,
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = { "lua_ls", "omnisharp", "clangd", "eslint", "jdtls", "jsonls", "markdown_oxide" },
+            })
+        end,
+    },
+    {
+        "neovim/nvim-lspconfig",
+        lazy = false,
+        config = function()
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({ capabilities = capabilities })
-			lspconfig.omnisharp.setup({ capabilities = capabilities })
-			lspconfig.clangd.setup({ capabilities = capabilities })
-			lspconfig.eslint.setup({ capabilities = capabilities })
-			lspconfig.jdtls.setup({ capabilities = capabilities })
-			lspconfig.jsonls.setup({ capabilities = capabilities })
-			lspconfig.markdown_oxide.setup({ capabilities = capabilities })
+            local lspconfig = require("lspconfig")
 
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-		end,
-	},
+            local mason_registry = require("mason-registry")
+
+            lspconfig.lua_ls.setup({
+                cmd = {
+                    mason_registry.get_package("lua-language-server"):get_install_path() ..
+                    "/bin/lua-language-server.exe",
+                },
+                capabilities = capabilities,
+            })
+            lspconfig.omnisharp.setup({
+                cmd = {
+                    mason_registry.get_package("omnisharp"):get_install_path() .. "/omnisharp.cmd"
+                },
+                capabilities = capabilities,
+            })
+            lspconfig.clangd.setup({ capabilities = capabilities })
+            lspconfig.jdtls.setup({ capabilities = capabilities })
+            lspconfig.jsonls.setup({ capabilities = capabilities })
+            lspconfig.markdown_oxide.setup({ capabilities = capabilities })
+
+            -- Keymaps **only after LSP initialization**
+            vim.api.nvim_create_autocmd("LspAttach", {
+                callback = function()
+                    vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+                    vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+                    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+                end,
+            })
+        end,
+    },
 }
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,7 +1,16 @@
-vim.opt.number = true                -- Show line numbers
-vim.opt.relativenumber = true        -- Show relative line numbers
-vim.opt.tabstop = 4                  -- Set tab width
-vim.opt.shiftwidth = 4               -- Set indentation width
-vim.opt.expandtab = true             -- Use spaces instead of tabs
-vim.opt.smartindent = true           -- Enable smart indentation
+vim.opt.number = true         -- Show line numbers
+vim.opt.relativenumber = true -- Show relative line numbers
+vim.opt.tabstop = 4           -- Set tab width
+vim.opt.shiftwidth = 4        -- Set indentation width
+vim.opt.expandtab = true      -- Use spaces instead of tabs
+vim.opt.smartindent = true    -- Enable smart indentation
 vim.g.mapleader = " "
+
+vim.api.nvim_set_keymap('i', '<A-n>', '<Esc>', { noremap = true })
+vim.api.nvim_set_keymap('v', '<A-n>', '<Esc>', { noremap = true })
+vim.api.nvim_set_keymap('c', '<A-n>', '<Esc>', { noremap = true })
+
+vim.api.nvim_create_user_command("OpenInChrome", function()
+    local file = vim.fn.expand('%:p')
+    vim.fn.jobstart({ "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe", file }, { detach = true })
+end, {})
